6350893206695f0d921449efced8585a520097b9
juliobana <47338088+juliobana@users.noreply.github.com>
2019-02-10 14:31:12 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -4,5 +4,17 @@ Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
 
+    var num1;
+    var num2;
+    var suma;
+
+    num1 = parseInt(document.getElementById("numeroUno").value);
+
+    num2 = parseInt(document.getElementById("numeroDos").value);
+
+    suma = num1 + num2;
+
+    alert("La suma es  " + suma);
+
 }
 
