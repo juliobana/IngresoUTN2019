b27c0b8491b9d06ec665211d3e7c369e2094dd52
juliobana <47338088+juliobana@users.noreply.github.com>
2019-02-13 22:17:27 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -3,4 +3,28 @@ function Mostrar()
 //tomo la edad  
 
 
+var edad;
+
+edad = parseInt(document.getElementById("edad").value);
+
+// Esta fuera del rango?
+
+if (!(edad <13 || edad >17))
+
+alert("Es adolecente")
+
+/*
+
+
+//Al ingresar una edad solo debemos informar si la persona NO es adolescente.
+var edad;
+
+edad = parseInt(document.getElementById("edad").value);
+
+// Esta fuera del rango?
+
+if (edad <13 || edad >17)
+
+alert("No es adolecente")
+*/
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,10 +1,27 @@
+
+//Al ingresar una edad debemos informar si la persona es mayor de edad
+// (mas de 18 años) o adolescente (entre 13 y 17 años) o niño (menor a 13 años).
 function Mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad = parseInt(document.getElementById("edad").value);
 
+if (edad >= 18)
 
+{
+    alert("Es mayor de edad");
+}
+    else if (edad < 13)
 
+        {
+         alert("Es niño");
+        }
+          
+        else
 
-
+             {
+             alert("Es adolecente");
+             }
 }//FIN DE LA FUNCIÓN
