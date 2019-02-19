153fcba2f2362dd9b42796fb9dc96f99ace32ab8
juliobana <47338088+juliobana@users.noreply.github.com>
2019-02-18 22:05:52 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,7 +1,36 @@
 function Mostrar()
 {
+
+
+    /*
+    al seleccionar un mes informar. 
+si es Enero: "que comiences bien el año!!!." 
+si es Marzo: "a clases!!!." 
+si es Julio: "se vienen las vacaciones!!!." 
+si es Diciembre: "Felices fiesta!!!."
+    */
 //tomo el mes
-var mesDelAño = document.getElementById('mes').value;
+var mesDelAnio = document.getElementById('mes').value;
+
+switch(mesDelAnio){
+
+case "Enero":
+alert("que comiences bien el año!!!." );
+break;
+
+case "Marzo":
+alert("a clases!!!." );
+break;
+
+case "Julio":
+alert("se vienen las vacaciones!!!." );
+break;
+
+case "Diciembre":
+alert("Felices fiesta!!!." );
+break;
+
+}
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,11 +1,39 @@
 function Mostrar()
 {
+/*
+al seleccionar un mes informar. 
+si estamos en Invierno: "Abrigate que hace frio." 
+si aún no llego el Invierno: "Falta para el invierno." 
+si ya paso el Invierno: "Ya pasamos el frio, ahora calor!!!." 
+ACLARAcIÓN: tomamos a Julio y Agosto como los meses de Invierno.
+*/
 //tomo el mes 
-var mesDelAño = document.getElementById('mes').value;
+var mesDelAnio = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch(mesDelAnio){
+case "Julio" : 
 
+alert("Abrigate que hace frio.");
 
+break;
 
+case "Agosto" : 
 
+alert("Abrigate que hace frio.");
+
+break;
+
+case "Mayo" : 
+
+alert("Falta para el invierno.");
+
+break;
+
+case "Diciembre" : 
+
+alert("Ya pasamos el frio, ahora calor!!!.");
+
+break;
+
+}
 }//FIN DE LA FUNCIÓN
