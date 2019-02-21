80dfc45c31fa15a538b67d364b8ea42d2bf4dcf2
juliobana <47338088+juliobana@users.noreply.github.com>
2019-02-20 22:03:06 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -3,7 +3,10 @@ function Mostrar()
 
 var sexo = prompt("ingrese f ó m .");
 
-
+while (!(sexo == "f" || sexo == "m"))
+{
+   sexo = prompt("Incorrecto ingrese f ó m .");
+}
 
 document.getElementById('Sexo').value=sexo;
 
