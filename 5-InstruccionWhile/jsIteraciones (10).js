9fb39c701874e19f66becc2ac23a897c752b67ec
juliobana <47338088+juliobana@users.noreply.github.com>
2019-02-27 21:21:39 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,16 +1,40 @@
 function Mostrar()
 {
 
-	var contador=0;
+	var numero;
+	var seguir;
+	var acumuladorPos=0;
+	var acumuladorNeg=0;
+	var contadorPos=0;
+	var contadorNeg=0;
+	contadorCeros=0;
+	var diferencia;
+	var promedioNeg=0;
+	var promedioPos=0;
+	
+
 	//declarar contadores y variables 
 	
 	var respuesta="si";
 
-	while(respuesta!="no")
-	{
+	do {
+		numero = parseInt(prompt("Ingrese un numero "));
+		while (isNaN(numero))
+			{
+				
+				
+				numero = parseInt(prompt("Esto no es un numero"));
+			}
+
 		
 	
-	}
+	
+	
+		respuesta = confirm("¿Desea continuar?");
+	  } while (respuesta == "s");
+
+	document.getElementById('suma').value = acumulador;
+	document.getElementById('promedio').value = acumulador / contador;
 
 
 
