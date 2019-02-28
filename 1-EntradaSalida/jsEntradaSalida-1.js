8d0ea819ad005c3c2eb85833c514767056f81d00
juliobana <47338088+juliobana@users.noreply.github.com>
2019-02-27 21:20:43 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,7 +1,48 @@
-//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+
+/*7- realizar el algoritmo que permita el ingreso por prompt de las notas (validar entre 0 y 10) y el sexo
+(validar el sexo “f” o “m”) de 100 alumnos, informar por alert:
+a) el promedio de las notas totales.
+b) la nota más baja.
+c) la cantidad de varones que su nota haya sido mayor o igual a 6. */
+
 function Mostrar()
 {
-	alert("Esto funciona de maravilla");
+var nota;
+var sexo;
+var promedio;	
+var notaBaja;
+var contadorVaronesMas5 = 0;
+var acumulador=0;
+var flag= 0;
+var sexoNotaBaja;
+var cantidad=3;
+
+for(var i = 0 ; i < cantidad; i++){
+
+	nota = parseInt(prompt("Ingrese nota: "));
+	while(nota < 0 || nota >10 || isNaN(nota)){
+		nota = parseInt(prompt("Error. Reingrese nota: "));
+	}
+
+	sexo = prompt("Ingrese sexo: ");
+	while(sexo != "f" && sexo !="m" ){
+		sexo = prompt("Error. Reingrese sexo: ");
+	}
+	acumulador = acumulador + nota;
+
+	if(sexo == "m" && nota >= 6){
+		contadorVaronesMas5++;
+	}
+
+	if(nota < notaBaja || flag==0)
+	{
+		notaBaja = nota;
+		sexoNotaBaja = sexo;
+		flag = 1;
+	}
+}
+	promedio = acumulador / cantidad;
+
+	alert("Promedio de las notas: " + promedio + "\nVarones con notas mayor: " + contadorVaronesMas5 +"\nNota mas baja: " + notaBaja + " \n Sexo nota mas baja: " + sexoNotaBaja );
 
 }
-
--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,13 +1,14 @@
-/*Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+/*8- realizar el algoritmo que permita ingresar números positivos (validar que sea positivo) hasta que el
+usuario quiera e informar al terminar el ingreso por document.write:
+a) la cantidad de números pares.
+b) el promedio de todos los números ingresados.
+c) la suma de todos los números.
+d) El número máximo y el mínimo.*/
+
 function Mostrar()
 {
 	
-var nombre;
 
-nombre = prompt("Ingrese su nombre");
-
-alert(nombre);
 
 }
 
